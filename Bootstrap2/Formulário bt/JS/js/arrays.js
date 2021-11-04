632d7dd11e20fd36e93c905ca6cae6ae9a73cbe5
Maarcela <marcela.vitoria277@gmail.com>
2021-11-04 14:54:25 -0300

    https://github.com/Maarcela/JS.git

---
+++ Bootstrap2/Formulário bt/JS/js/arrays.js
@@ -0,0 +1,51 @@
+/* unshift, Entra Amanda*/
+const festa1 = ['Joaquim', 'José', 'Silva', 'Xavier'];
+let w = festa1.unshift('Amanda'); // w = 5
+
+console.log(festa1);
+console.log(w);
+
+/*Xavier saiu*/
+const festa2 = ['Amanda', 'Joaquim', 'José', 'Silva', 'Xavier'];
+let x = festa2.pop(); // x = 'Xavier'
+
+console.log(festa2);
+console.log(x);
+
+/*push, chegou Zuleica*/
+console.clear();
+const festa3 = ['Amanda', 'Joaquim', 'José', 'Silva'];
+let y = festa3.push('Zuleica'); // y = 5
+
+console.log(festa3);
+console.log(y);
+
+/*Amanda saiu*/
+
+/* shift, Amanda saiu*/
+console.clear();
+const festa4 = ['Amanda', 'Joaquim', 'José', 'Silva'];
+let z = festa4.shift(); // z = 'Amanda'
+
+console.log(festa4);
+console.log(z);
+
+/* shift, Silva saiu*/
+console.clear();
+const festa4 = ['Amanda', 'Joaquim', 'José', 'Silva'];
+let z = frutas3.shift(); // z = 'Silva'*/
+
+/* delete, Silva*/
+console.clear();
+const festa5 = ['Amanda', 'Joaquim', 'José', 'Silva'];
+delete festa5[3];
+
+console.log(festa5);
+
+/* push, Silvana entrou*/
+console.clear();
+const festa6 = ['Joaquim', 'José'];
+let y = festa5.push('Kiwi'); // y = 2
+
+console.log(frutas2);
+console.log(y);
