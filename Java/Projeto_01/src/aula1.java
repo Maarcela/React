cd9f0fd74308cceffe66b59321ca585b32748056
Maarcela <91353886+Maarcela@users.noreply.github.com>
2021-12-19 14:22:47 -0300

    Delete Java directory

--- Java/Projeto_01/src/aula1.java
+++
@@ -1,8 +0,0 @@
-public class aula1 {
-    // Atividade Java
-    public static void main(String[] args) {
-        System.out.println("Marcela Vitoria");
-        System.out.println("São Paulo");
-        System.out.println("Ler");
-    }
-}
--- Java/Projeto_02/src/Scanner.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-//1.	Faça um algoritmo que leia um número inteiro.
-//•	Se o número for maior que 20. . .
-//•	calcular e imprimir a metade dele.
-
-//public class Scanner; {
-    //public static void main (String[] args){}
-
